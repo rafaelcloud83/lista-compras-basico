41b8632db1b8e6f2a9f7a228eb1c0f34771642dc
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-10-29 20:18:13 -0300

    criado componente, html e scss compras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ComprasComponent } from "./compras/compras.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
