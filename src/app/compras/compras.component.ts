adcf4b7a77fb5a9ddaa298972da5e3736e2d559f
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-10-29 21:09:50 -0300

    implementado funcionalidade de adicionar item, riscar item comprado e limpar a lista

--- src/app/compras/compras.component.ts
+++ src/app/compras/compras.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-compras',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './compras.component.html',
   styleUrl: './compras.component.scss'
 })
 export class ComprasComponent {
 
+  item: string = '';
+  lista: ItemLista[] = [];
+
+  adicionarItem() {
+    //console.log("Item adicionado:", this.item);
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+
+    //console.table(this.lista);
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+  limparLista() {
+    this.lista = [];
+  }
 }
